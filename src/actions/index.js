9310f40b91368475d5ad39e580ffcc236e4ca8f7
rito <59119963+pl-pr-pm@users.noreply.github.com>
2022-01-13 15:32:26 +0900

    connect apiを利用したstate, actionsとの関連づけ

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,9 @@
 export const INCREMENT = "INCREMENT";
 export const DECREMENT = "DECREMENT";
 
-export const increment = () => ({ type: INCREMENT });
+export const increment = () => {
+  return { type: INCREMENT };
+};
 
 export const decrement = () => ({
   type: DECREMENT
--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,28 @@
-import "./styles.css";
+import { connect } from "react-redux";
+import React from "react";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+import { increment, decrement } from "../actions/index";
+
+class App extends React.Component {
+  render() {
+    const props = this.props;
+    // props.increment => object
+    console.log(props.increment);
+    return (
+      <>
+        <div>count: {props.value}</div>
+        <button onClick={props.increment}>+1</button>
+        <button onClick={props.decrement}>-1</button>
+      </>
+    );
+  }
 }
+
+const mapStateToProps = (state) => ({ value: state.count.value });
+
+const mapDispatchToProps = (dispatch) => ({
+  increment: () => dispatch(increment()),
+  decrement: () => dispatch(decrement())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
